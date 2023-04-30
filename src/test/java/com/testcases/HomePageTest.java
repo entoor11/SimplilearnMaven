af4b768f4b440e984db0c0cd709eb0d5e9b0e20a
eclipse <eclipse@example.com>
2023-04-30 16:31:29 +1000

    third push

--- src/test/java/com/testcases/HomePageTest.java
+++ src/test/java/com/testcases/HomePageTest.java
@@ -9,24 +9,16 @@ import com.pages.LoginPage;
 
 public class HomePageTest extends BaseClass{
 	
-	@Test
-	public void test3() {
-		System.out.print("Test Three");
-	}
+//	@Test
+//	public void test3() {
+//		System.out.print("Test Three");
+//	}
 
-	
-	@Test
-	public void test1() {
+	@Test(priority = 2)
+	public void test2() {
 
 		LoginPage login = new LoginPage(driver);
-		login.LoginFunction("abc@xyz.com", "Abc@1234");
-
-		WebElement error = driver.findElement(By.id("msg_box"));
-
-		String ActError = error.getText();
-		String ExpError = "The email or password you have entered is invalid.";
-
-		Assert.assertEquals(ActError, ExpError);
+		login.LoginFunction("homepage@xyz.com", "Abc@1234");
 
 	}
 }
--- src/test/java/com/testcases/LoginTestCase.java
+++ src/test/java/com/testcases/LoginTestCase.java
@@ -15,46 +15,46 @@ import com.pages.LoginPage;
 
 public class LoginTestCase extends BaseClass {
 
-	@Test(priority = 1)
-	public void test1() {
-
-		LoginPage login = new LoginPage(driver);
-		login.LoginFunction("abc@xyz.com", "Abc@1234");
-
-		WebElement error = driver.findElement(By.id("msg_box"));
-
-		String ActError = error.getText();
-		String ExpError = "The email or password you have entered is invalid.";
-
-		Assert.assertEquals(ActError, ExpError);
-
-	}
+//	@Test(priority = 1)
+//	public void test1() {
+//
+//		LoginPage login = new LoginPage(driver);
+//		login.LoginFunction("abc@xyz.com", "Abc@1234");
+//
+//		WebElement error = driver.findElement(By.id("msg_box"));
+//
+//		String ActError = error.getText();
+//		String ExpError = "The email or password you have entered is invalid.";
+//
+//		Assert.assertEquals(ActError, ExpError);
+//
+//	}
 
 	@Test(priority = 2)
 	public void test2() {
 
 		LoginPage login = new LoginPage(driver);
-		login.LoginFunction("pqr@xyz.com", "Abc@1234");
+		login.LoginFunction("Logintest@xyz.com", "Abc@1234");
 
 	}
 
 	
-	//Method name test4 should be same as test4 in excel data
-	@Test(priority = 3)
-	public void test4(Method method) throws FilloException {
-
-		LoginPage login = new LoginPage(driver);
-		//Recordset recordset = connection.executeQuery("select * from data where TestName='test4'");
-		Recordset recordset = connection.executeQuery("select * from data where TestName='"+method.getName()+"'");
-		
-		recordset.next();
-		
-		String UserName = recordset.getField("UserName");
-		String Password = recordset.getField("Password");
-
-		login.LoginFunction(UserName, Password);
-
-	}
+//	//Method name test4 should be same as test4 in excel data
+//	@Test(priority = 3)
+//	public void test4(Method method) throws FilloException {
+//
+//		LoginPage login = new LoginPage(driver);
+//		//Recordset recordset = connection.executeQuery("select * from data where TestName='test4'");
+//		Recordset recordset = connection.executeQuery("select * from data where TestName='"+method.getName()+"'");
+//		
+//		recordset.next();
+//		
+//		String UserName = recordset.getField("UserName");
+//		String Password = recordset.getField("Password");
+//
+//		login.LoginFunction(UserName, Password);
+//
+//	}
 	
 	
 
